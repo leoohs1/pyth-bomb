6851a9e634fc9d2c72ec54c60a49880ba50cd0e3
leoohs1 <leonardo.hs1@outlook.comm>
2026-09-22 11:03:23 -0300

    Landing Coming Soon e jogo em /play

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Landing from './Landing.jsx'
+
+const isGame = window.location.pathname.startsWith('/play')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isGame ? <App /> : <Landing />}
   </StrictMode>,
 )
